f538faa4b44c03fd7b86b074f3f923a7f9785ce0
Rohan Kumara <31560310+rohankumara@users.noreply.github.com>
2021-08-21 00:56:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+def npr(image_path):
     import cv2 as cv
     import imutils
     import numpy as np
     import pytesseract as tess
 
     tess.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-    img = cv.imread('02.jpeg', cv.IMREAD_COLOR)
+    
+    # image read and resize
+    img = cv.imread(image_path, cv.IMREAD_COLOR) 
     img = cv.resize(img, (400, 200))
 
+    # 
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = cv.bilateralFilter(gray, 13, 15, 15)
 
@@ -42,17 +32,10 @@ if __name__ == '__main__':
             screenCnt.append(approx)
     text = []
     if len(screenCnt) != 0:
-        print(len(screenCnt))
         for sc in screenCnt:
             xd = cv.drawContours(img, [sc], -1, (0, 0, 255), 3)
-            # text.append(tess.image_to_string(xd))
-            # cv.imshow('Cropped', xd)
-            # cv.waitKey(0)
-            # cv.destroyAllWindows()
-        print(text)
     else:
-        final = tess.image_to_string(gray)
-        print(final)
+        print("No Results")
  
 
 
@@ -65,20 +48,17 @@ if __name__ == '__main__':
         (bottomx, bottomy) = (np.max(x), np.max(y))
         Cropped = gray[topx:bottomx + 1, topy:bottomy + 1]
 
-        text.append(tess.image_to_string(Cropped))
+        fine = tess.image_to_string(Cropped)
+        text.append(fine.replace('\x0c','')) # clean output
         cv.imshow('Cropped', Cropped)
         cv.waitKey(0)
         cv.destroyAllWindows()
-    print(text)
-    # text = pytesseract.image_to_string(Cropped, config='--psm 11')
-    # print("programming_fever's License Plate Recognition\n")
-    # print("Detected license plate Number is:", text)
-    # img = cv.resize(img, (500, 300))
-    # Cropped = cv.resize(Cropped, (400, 200))
-    # cv.imshow('car', img)
-    # cv.imshow('Cropped', Cropped)
-    #
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
+    if len(text) == 0 or text == '' or text[0]=='':
+        print("Sory Can't Identify number plate!!!")
+    else:
+        print("Number plate should be >>",text)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == '__main__':
+    image_path = str(input("Enter image parth : "))
+    npr(image_path)
